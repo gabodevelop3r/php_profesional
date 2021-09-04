2825a093f1b0f6d9a601d57f4f828191ad83e909
Gabo-dev <gabrielgallegos@MacBook-Pro-2.local>
2021-09-04 11:33:49 -0400

    [ADD] Comillas simples y dobles

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php 
+
+    /***
+     *  MOSTRAR DATOS  
+     * 
+     */
     echo "hola ","mundo";
     print 'hola';
+
+    /***
+     *  COMO UTILIZAR COMILLAS
+     * 
+     */
+    $animal = "panda";
+    echo "HOLA \"$animal \n adios\"";
+    echo "Tu ganas mucho dinero \$_$";
+    echo 'Hola $animal \n "comillas " \'T\'';
 
 ?>
