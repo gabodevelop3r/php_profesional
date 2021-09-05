256b82f5b5d684e92f00da925fbfddd8209be1f7
Gabo-dev <gabrielgallegos@MacBook-Pro-2.local>
2021-09-04 21:58:44 -0400

    [ADD] Asignacion

--- operadores.php
+++ operadores.php
@@ -1,41 +1,70 @@
 <?php
 
-$age = '19';
+    /** COMPARACION */
+
+    $age = '19';
+
+    /**
+     * 
+     *  == Igual
+     *  echo var_dump($age == 18);
+     * 
+     *  === Identico
+     *  echo var_dump( $age === 18 );
+     * 
+     *  !=  <> Diferente
+     *  echo var_dump( $age <> 18 );
+     * 
+     *  !== No identico
+     *  echo var_dump( $age !== 18 );
+     * 
+     *  > mayor que
+     *  echo var_dump( $age > 18 );
+     * 
+     *  < Menor que 
+     *  echo var_dump( $age > 18 );
+     *  
+     *  <= Menor o igual que
+     *  echo var_dump( $age <= 18 );
+     *   
+     *  >= Mayor o igual que
+     *  echo var_dump( $age >= 18 );
+     *  
+     *  <=> Nave espacial ( retorna -1 si el valor es menor , 0 si el valor es igual y 1 si el valor es mayor )
+     *  echo var_dump( $age <=> 18 );
+     * 
+     */
 
 
-echo var_dump( $age <=> 18 );
+    /*** ARITMETICOS */
 
-/**
- * 
- *  == Igual
- *  echo var_dump($age == 18);
- * 
- *  === Identico
- *  echo var_dump( $age === 18 );
- * 
- *  !=  <> Diferente
- *  echo var_dump( $age <> 18 );
- * 
- *  !== No identico
- *  echo var_dump( $age !== 18 );
- * 
- *  > mayor que
- *  echo var_dump( $age > 18 );
- * 
- *  < Menor que 
- *  echo var_dump( $age > 18 );
- *  
- *  <= Menor o igual que
- *  echo var_dump( $age <= 18 );
- *   
- *  >= Mayor o igual que
- *  echo var_dump( $age >= 18 );
- *  
- *  <=> Nave espacial ( retorna -1 si el valor es menor , 0 si el valor es igual y 1 si el valor es mayor )
- *  echo var_dump( $age <=> 18 );
- * 
- */
+    $operando1 = 2;
+    $operando2 = 2;
 
+    $result = $operando1 ** $operando2;
+    echo $result;
+    /***
+     * 
+     *  Adicion +
+     *  $result = $operando1 + $operando2;
+     * 
+     *  Sustraccion
+     *  $result = $operando1 - $operando2;
+     * 
+     *  Multiplicacion *
+     *  $result = $operando1 * $operando2;
+     * 
+     *  Division
+     *  $result = $operando1 / $operando2;
+     * 
+     *  Modulo
+     *  $residuo = $operando1 % $operando2;
+     * 
+     *  Exponenciacion
+     *  $result = $operando1 ** $operando2;
+     *  
+     * 
+     */
 
 
 ?>
