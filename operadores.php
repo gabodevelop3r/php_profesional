9b0157cdf42a1dcd2402dde9b26d7288d429f4c0
Gabo-dev <gabrielgallegos@MacBook-Pro-2.local>
2021-09-04 21:43:26 -0400

    [ADD] se cambio archivo index por operadores y variables, [ADD] operadores (comparacion)

---
+++ operadores.php
@@ -0,0 +1,41 @@
+<?php
+
+$age = '19';
+
+
+echo var_dump( $age <=> 18 );
+
+/**
+ * 
+ *  == Igual
+ *  echo var_dump($age == 18);
+ * 
+ *  === Identico
+ *  echo var_dump( $age === 18 );
+ * 
+ *  !=  <> Diferente
+ *  echo var_dump( $age <> 18 );
+ * 
+ *  !== No identico
+ *  echo var_dump( $age !== 18 );
+ * 
+ *  > mayor que
+ *  echo var_dump( $age > 18 );
+ * 
+ *  < Menor que 
+ *  echo var_dump( $age > 18 );
+ *  
+ *  <= Menor o igual que
+ *  echo var_dump( $age <= 18 );
+ *   
+ *  >= Mayor o igual que
+ *  echo var_dump( $age >= 18 );
+ *  
+ *  <=> Nave espacial ( retorna -1 si el valor es menor , 0 si el valor es igual y 1 si el valor es mayor )
+ *  echo var_dump( $age <=> 18 );
+ * 
+ */
+
+
+
+?>
