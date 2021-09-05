1d1bf430dcc2d4625cabddd2636b977cfea44ab6
Gabo-dev <gabrielgallegos@MacBook-Pro-2.local>
2021-09-05 11:33:29 -0300

    [ADD] for

--- condicionesyciclos.php
+++ condicionesyciclos.php
@@ -1,37 +1,43 @@
 <?php
 
-$age = 40;
+    $age = 40;
 
-if( $age >= 18 && $age <= 30):
-    //sentencia
-    // echo "Puedes ver la pelicula";
-elseif( $age > 30 ):
-    // echo "No puedes ver la pelicula eres mayor a 30";
+    if( $age >= 18 && $age <= 30):
+        //sentencia
+        // echo "Puedes ver la pelicula";
+    elseif( $age > 30 ):
+        // echo "No puedes ver la pelicula eres mayor a 30";
 
-else:
-    // echo "No puedes ver la pelicula, tu edad es menor a 18";
-    // echo "No puedes ver la pelicula";
-endif;
+    else:
+        // echo "No puedes ver la pelicula, tu edad es menor a 18";
+        // echo "No puedes ver la pelicula";
+    endif;
 
-$shirt = 'Rojo';
+    $shirt = 'Rojo';
 
-switch ($shirt):
-    case 'Rojo':
-        echo "Color rojo";
-        break;
-    case 'Azul':
-        echo "Color Azul";
-        break;
-    case 'Rosa':
-        echo "Color Rosa";
-        break;
-    case 'Amarillo':
-        echo "Color Amarillo";
-        break;
-    default:
-        echo "No encontramos lo que buscabas";
-        break;
+    switch ($shirt):
+        case 'Rojo':
+            echo "Color rojo";
+            break;
+        case 'Azul':
+            echo "Color Azul";
+            break;
+        case 'Rosa':
+            echo "Color Rosa";
+            break;
+        case 'Amarillo':
+            echo "Color Amarillo";
+            break;
+        default:
+            echo "No encontramos lo que buscabas";
+            break;
     endswitch;
+    echo "<br>";
+
+    for ( $i = 10; $i >= 1 ; --$i ) { 
+        # code...
+        echo "Anuncio a repetir $i <br>";
+    }
 
 
 
