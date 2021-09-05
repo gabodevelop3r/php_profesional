d56694beaa7fc12a4d44b32ec76348cafd9acb05
Gabo-dev <gabrielgallegos@MacBook-Pro-2.local>
2021-09-05 11:21:39 -0300

    [ADD] switch

--- condicionesyciclos.php
+++ condicionesyciclos.php
@@ -4,14 +4,34 @@ $age = 40;
 
 if( $age >= 18 && $age <= 30):
     //sentencia
-    echo "Puedes ver la pelicula";
+    // echo "Puedes ver la pelicula";
 elseif( $age > 30 ):
-    echo "No puedes ver la pelicula eres mayor a 30";
+    // echo "No puedes ver la pelicula eres mayor a 30";
 
 else:
-    echo "No puedes ver la pelicula, tu edad es menor a 18";
-    echo "No puedes ver la pelicula";
-endif
+    // echo "No puedes ver la pelicula, tu edad es menor a 18";
+    // echo "No puedes ver la pelicula";
+endif;
+
+$shirt = 'Rojo';
+
+switch ($shirt):
+    case 'Rojo':
+        echo "Color rojo";
+        break;
+    case 'Azul':
+        echo "Color Azul";
+        break;
+    case 'Rosa':
+        echo "Color Rosa";
+        break;
+    case 'Amarillo':
+        echo "Color Amarillo";
+        break;
+    default:
+        echo "No encontramos lo que buscabas";
+        break;
+    endswitch;
 
 
 
