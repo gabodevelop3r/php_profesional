ee444bc0e4a8ed383bad63e5a4ca6ce6be8a5f3c
Gabo-dev <gabrielgallegos@MacBook-Pro-2.local>
2021-09-17 17:00:30 -0300

    [ADD] clases y atributos

---
+++ poo/person.php
@@ -0,0 +1,29 @@
+<?php
+
+class Person{
+
+    $name;
+    $lastname;
+    $sex;
+    $nationality;
+    $age;
+
+    function run(){
+
+    }
+
+    function see(){
+        echo "yo veo una camisa azul";
+        $this->name;
+        $this->run();
+    }
+
+    function talk(){
+        $this->see();
+        $this->lastname;
+    }
+
+}
+
+
+?>
