b3c999f06c50c2c0e1020d9340b4a148a741700c
Gabo-dev <gabrielgallegos@MacBook-Pro-2.local>
2021-09-17 16:35:09 -0300

    [ADD] argumentos de funciones

---
+++ funciones.php
@@ -0,0 +1,54 @@
+<?php
+
+
+function subjects(){
+
+    echo "Matematicas, Biologia, Español, Ciencias, Geografia";
+}
+
+# echo "Jose tus materias son: ";
+# subjects();
+
+# echo "<br> Hola amiga espero y estes bien tus materias son";
+# subjects();
+
+
+/*** Argumentos de funciones  */
+
+function divideBy( $divisor, $dividendo = 2 ){
+
+    $result = $divisor/$dividendo;
+    echo " El resultado de la division es: $result";
+
+}
+
+# divideBy( 15, 5 );
+
+/*** Argumentos por referencia */
+function plus ( $param1 , $param2 ){
+
+    $param1 = $param1 + $param2;
+
+    echo "El resultado de la suma es $param1";
+}
+
+$arg1 = 20 ;
+$arg2 = 5;
+
+plus( $arg1, $arg2 );
+
+# echo "\n El argumento es $arg1";
+
+
+/*** funciones recursivas */
+
+function call ( $value ){
+    if ( $value <= 10 ) {
+        # code...
+        echo "$value \n";
+        call( ++$value );
+    }
+}
+call( 1 );
+
+?>
