734fa525df172ff70b285a8fd8d0f88abddcd88a
Gabo-dev <gabrielgallegos@MacBook-Pro-2.local>
2021-09-17 16:07:28 -0300

    [ADD] arreglos multidimensional

--- Arreglos.php
+++ Arreglos.php
@@ -18,11 +18,29 @@ $person = array(
             'age' => 123
         );
 
-print_r( $person );
+# print_r( $person );
 
-echo $person['lastname'];
+# echo $person['lastname'];
 $person['lastname'] = 'gallegos';
-print_r( $person );
+# print_r( $person );
+
+
+/***Arreglos multidimensionales */
+
+$battleship = array( 
+                    'A' => array('Mar','BARCO','Mar','Mar'),
+                    'B' => array('Mar','Mar','Mar','Mar'),
+                    'C' => array('Mar','Mar','Mar','Mar'),
+                    'D' => array('Mar','Mar','Mar','Mar')
+                );
+
+print_r( $battleship );
+
+$coodinates = $battleship['A'][0];
+echo $coodinates;
+
+$coodinates = $battleship['A'][1];
+echo $coodinates;
 
 
 
